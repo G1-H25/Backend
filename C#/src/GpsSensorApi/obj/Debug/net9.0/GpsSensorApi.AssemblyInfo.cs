f2c846511965d5fb2f6427f97bd09107c1f32abf
Wilmer-Chas <wilmer.forsman@chasacademy.se>
2025-08-22 19:27:46 +0200

    Pain, suffering, 1 hour, 30 minutes of debugging to get the damnable package called crow to work

--- C#/src/GpsSensorApi/obj/Debug/net9.0/GpsSensorApi.AssemblyInfo.cs
+++ C#/src/GpsSensorApi/obj/Debug/net9.0/GpsSensorApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GpsSensorApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76d74343b20da5eb664b4128bc0af21c2db30486")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8af49a0a29a4fcee853b63eabd2c24b7e2f91b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GpsSensorApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GpsSensorApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
