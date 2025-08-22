42cc5c09eac88dfc5cca82511b571ef03c101ecc
Wilmer-Chas <wilmer.forsman@chasacademy.se>
2025-08-22 19:59:39 +0200

    added post for demonstration

--- CPlusPlus/src/main.cpp
+++ CPlusPlus/src/main.cpp
@@ -10,5 +10,15 @@ int main() {
         return "Hello, Crow!";
     });
 
+    // curl -X POST http://localhost:18080/json      -H "Content-Type: application/json"      -d '{"name": "ERIKA"}'
+    CROW_ROUTE(app, "/json").methods(crow::HTTPMethod::Post)([](const crow::request& req) {
+        auto json = crow::json::load(req.body);
+        if (!json)
+            return crow::response(400, "Invalid JSON");
+
+        std::string name = json["name"].s();
+        return crow::response("Hello, " + name + "!");
+    });
+
     app.port(18080).run();
 }
