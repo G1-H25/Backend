f37c5cc7f18e98aa4f33d919488d106f1ad36b74
bubba-94 <carljohan.modin@gmail.com>
2025-09-08 15:19:46 +0200

    Tests for sql files

--- sql/test.sql
+++ sql/test.sql
@@ -1,3 +1,18 @@
 CREATE TABLE testtable (
     personID INT,
 );
+
+-- BAD: SELECT * usage
+SELECT * FROM testtable;
+
+-- BAD: missing semicolon
+SELECT personID FROM testtable
+
+-- BAD: lowercase keyword
+select personID FROM testtable;
+
+-- BAD: alias without AS
+SELECT personID my_id FROM testtable;
+
+-- GOOD: clean select with alias and semicolon
+SELECT personID AS my_id FROM testtable;
