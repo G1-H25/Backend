5d6ec926170d542a934e8aaade8d508caf5aa43d
bubba-94 <carljohan.modin@gmail.com>
2025-09-08 20:25:38 +0200

    Ran tests locally and it worked
    - checking if workflow works on push
    - add sqlfluff.md for easy access to commands
    - add dev/** so that lint runs on dev/ branches

--- sql/test.sql
+++ sql/test.sql
@@ -1,18 +1,4 @@
-CREATE TABLE testtable (
-    personID INT,
-);
-
--- BAD: SELECT * usage
-SELECT * FROM testtable;
-
--- BAD: missing semicolon
-SELECT personID FROM testtable
-
--- BAD: lowercase keyword
-select personID FROM testtable;
-
--- BAD: alias without AS
-SELECT personID my_id FROM testtable;
-
--- GOOD: clean select with alias and semicolon
-SELECT personID AS my_id FROM testtable;
+SELECT
+    c AS bar,
+    a + b AS foo
+FROM my_table
