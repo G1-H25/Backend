09ab5407292f6be47809fdb9d2a0e2561f9998a4
Sand <sandra.obradovic@chasacademy.se>
2025-09-19 11:29:32 +0200

    creating tables contact, customer and company

--- scripts/sql/company.sql
+++ scripts/sql/company.sql
@@ -1,6 +1,5 @@
 CREATE TABLE Company (
     PK INT IDENTITY(1,1) PRIMARY KEY,
-    Id INT IDENTITY(1,1) UNIQUE, -- You usually only need one identity, but included as requested
     Name VARCHAR(100),
     Email VARCHAR(100),
     Street VARCHAR(40),
