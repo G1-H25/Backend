c8ecc25be9f71b1c322013bbdf8900ba8ffbf9f8
Wilmer-Chas <wilmer.forsman@chasacademy.se>
2025-09-23 10:40:01 +0200

    test fix for error CS106

---
+++ GpsApp/compostion/SwaggerSetup.cs
@@ -0,0 +1,22 @@
+// Composition/SwaggerSetup.cs
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace GpsApp.Composition  
+{
+    public static class SwaggerSetup
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFilename);
+                options.IncludeXmlComments(xmlPath);
+            });
+
+            return services;
+        }
+    }
+}
