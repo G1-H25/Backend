23aec13ab1300ef7ea315b7c14895943800eba63
Sand <sandra.obradovic@chasacademy.se>
2025-09-23 12:01:36 +0200

    Add Logistics related tables and SQL scripts, first draft

---
+++ scripts/sql/3_Logistics/01_Tables/vehicle.sql
@@ -0,0 +1,10 @@
+IF OBJECT_ID('Vehicle', 'U') IS NULL
+BEGIN
+    CREATE TABLE Vehicle(
+        Id INT IDENTITY(1,1),
+        Type VARCHAR (20),
+        Registration VARCHAR (30),  
+        PRIMARY KEY (Id)
+        )
+END
+GO
