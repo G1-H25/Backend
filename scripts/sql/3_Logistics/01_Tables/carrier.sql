cc5a256ec03dd9d766cb4b0b2c9329dcea7ee722
Sand <sandra.oobradovic@gmail.com>
2025-09-29 09:07:11 +0200

    Dev/logistics/tables to  dev/us-007-forming-database-tables (#106)
    * Add Logistics related tables and SQL scripts, first draft
    
    * deleted delivery and delivery_state

---
+++ scripts/sql/3_Logistics/01_Tables/carrier.sql
@@ -0,0 +1,11 @@
+IF OBJECT_ID('Carrier', 'U') IS NULL
+BEGIN
+    CREATE TABLE Carrier(
+        Id INT IDENTITY(1,1),
+        Type VARCHAR(20), 
+        PRIMARY KEY (Id), 
+        FOREIGN KEY (VehicleId) REFERENCES Vehicle(Id), 
+        FOREIGN KEY (Contact) REFERENCES Company (Contact_Id)
+    )
+END
+GO
