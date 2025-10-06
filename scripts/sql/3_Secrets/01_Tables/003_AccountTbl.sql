5e203426fffd13df7a9c55e7b5fb0f65bdc2cd54
Wilmer-Chas <wilmer.forsman@chasacademy.se>
2025-10-06 10:37:14 +0200

    cleaning up database tables

--- scripts/sql/3_Secrets/01_Tables/003_AccountTbl.sql
+++ scripts/sql/3_Secrets/01_Tables/003_AccountTbl.sql
@@ -2,24 +2,22 @@ IF OBJECT_ID('Secrets.Account', 'U') IS NULL
     BEGIN
         CREATE TABLE Secrets.Account
         (
-            Id INT IDENTITY (1, 1) PRIMARY KEY,
-            UserName VARCHAR(30) NOT NULL,
-            UserPassword VARCHAR(30) NOT NULL,
-            CreatedAt DATETIME2,
+            Id INT IDENTITY(1,1) PRIMARY KEY,
+            Username VARCHAR(100) NOT NULL UNIQUE,
+            Password VARCHAR(100) NOT NULL,
+            Role VARCHAR(50) NOT NULL DEFAULT 'User',
+            DateCreated DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
             Note VARCHAR(50)
         );
     END
 
 -- SEED DATA 
 
-IF
-    NOT EXISTS (
-        SELECT 1
-        FROM Secrets.Account
-        WHERE UserName = 'Admin'
-    )
-    BEGIN
-        INSERT INTO Secrets.Account (UserName, UserPassword, CreatedAt, Note)
-        VALUES ('Admin', 'hello!', '2025-10-04T14:30:00', 'Test admin')
-    END
+IF NOT EXISTS (
+    SELECT 1 FROM Secrets.Account WHERE Username = 'Admin'
+)
+BEGIN
+    INSERT INTO Secrets.Account (Username, Password, Role, DateCreated, Note)
+    VALUES ('Admin', 'hello!', 'Admin', '2025-10-04T14:30:00', 'Test admin');
+END
 GO
