e3024f669bee1c4eb018370a2afa14f4e44e5921
Wilmer-Chas <wilmer.forsman@chasacademy.se>
2025-10-07 13:49:03 +0200

    fixed C# linting

--- GpsApp/model/deliveryData.cs
+++ GpsApp/model/deliveryData.cs
@@ -1,11 +1,11 @@
-public record DeliveryData (
-    int? DeliveryId, 
-    int? RouteId, 
-    int? ExpectedTempId, 
-    int? ExpectedHumidId, 
-    int? CarrierId, 
-    int? SenderId, 
+public record DeliveryData(
+    int? DeliveryId,
+    int? RouteId,
+    int? ExpectedTempId,
+    int? ExpectedHumidId,
+    int? CarrierId,
+    int? SenderId,
     int? RecipientId,
-    int? StateId, 
+    int? StateId,
     DateTime? OrderPlaced
 );
