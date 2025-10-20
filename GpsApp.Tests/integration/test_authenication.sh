3c8567f44905c58d8aa31514880869f8b60944ab
Wilmer-Chas <wilmer.forsman@chasacademy.se>
2025-10-20 12:12:06 +0200

    very confused why the tests failed

--- GpsApp.Tests/integration/test_authenication.sh
+++ GpsApp.Tests/integration/test_authenication.sh
@@ -109,7 +109,7 @@ echo "Registering device..."
 register_response=$(curl -s -w "\nHTTP Status: %{http_code}\n" -X POST http://localhost:5000/Gateway/register \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $TOKEN" \
-  -d '{"deviceId":1}')
+  -d "{"deviceId":1}")
 
 echo "Register response:"
 echo "$register_response"
