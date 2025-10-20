20e31a76d89270303d3d54b97a030620d1232b09
Wilmer-Chas <wilmer.forsman@chasacademy.se>
2025-10-20 11:54:13 +0200

    forgot to add new files

---
+++ GpsApp/DTO/GatewayInsertRequest.cs
@@ -0,0 +1,20 @@
+namespace GpsApp.DTO
+{
+    /*
+            CREATE TABLE Secrets.Gateway
+    (
+        Id INT IDENTITY (1, 1) PRIMARY KEY,
+        GatewayURL VARCHAR(50),
+        UserId INT NULL CONSTRAINT FK_Gateway_UserId
+            FOREIGN KEY (UserId) REFERENCES Secrets.Account(Id),
+        CurrentLocationId INT NULL CONSTRAINT FK_Gateway_CurrentLocationId
+            FOREIGN KEY (CurrentLocationId) REFERENCES Secrets.LocationHistory(Id)
+    )
+    */
+    public class GatewayInsertRequest
+    {
+        public int GatewayId { get; set; }
+        public string? GatewayURL { get; set; }
+        public int? CurrentLocationId { get; set; }
+    }
+}
